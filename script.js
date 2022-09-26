539a222b25e97405cfdc5407905a8f04aaa76a4e
Dev_matth <45796720+matheusjean28@users.noreply.github.com>
2022-09-26 17:42:31 -0300

    salvando dados em objeto
    dados salvos sem tratativa

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 
-let nome = document.getElementById("name").value
-const enviar = document.getElementById("submit")
-const lista = document.getElementById("ul-lista")
-const price = document.getElementById("price").value
-const date = document.getElementById("data").value
-// criar a li 
+// pegando informaçoes 
+let enviar = document.getElementById('submit')
+let nome = document.getElementById('name')
+let data = document.getElementById('data')
+let preco = document.getElementById('price')
+
+//lista
+let list = document.getElementById('ul-lista')
 
 
-enviar.addEventListener("click", (e) => {
+//funcoes uteis 
+let arraydedados = []
+
+function limparInputs() {
+    nome.value = ''
+    data.value = ''
+    price.value = ''
+}
+
+function juntardados() {
+    let name = nome.value;
+    let date = data.value
+    let preco = price.value
+    
+
+        dadosData = { name, date, preco }
+        arraydedados.push(dadosData)
+
+        return [arraydedados]
+
+
+}
+let resultadodafuncao = juntardados()
+//
+
+//criar elemento 
+function criarElement() {
+    let item = document.createElement('li')
+    list.appendChild(item)
+    item.classList.add('pago')
+    item.innerHTML = `<button id="btnOk"><img src="/evento.png" alt=""></button><p>${nome.value}</p><p>${data.value}</p><p id="valor"><b></b>${price.value}</p><img src="/more.png" alt="">`
+    list.appendChild(item)
+    limparInputs()
+}
+
+
+//eventos
+enviar.addEventListener('click', (e) => {
     e.preventDefault()
-        const elemento = document.createElement('li')
-        // adicionar o elemento html 
-        elemento.innerHTML = `<div class="img-item"><img src="/src/cronometro.png" alt=""></div><div class="tag-item">${nome}</div><div class="date-item">${date}</div><div class="price-item"><b>R$_</b><P>${price}</p><div class="more-item"><img src="/more.png" alt=""></div>`
-        elemento.classList.add("item-lista-boleto")
-        lista.appendChild(elemento)
-    // adicionar a classe do elemento
+    juntardados()
+    criarElement()
+    console.log(resultadodafuncao[0])
+
 })
 
